566cfe62c23b9dff902cf1c4e26b917d3dc167d8
Posey <adeposi60@gmail.com>
2021-03-06 04:48:59 +0100

    Created lessons.js file

---
+++ lessons.js
@@ -0,0 +1,85 @@
+let lessons = [
+    {
+      id:1,
+      subject:'Math',
+    location:'London',
+    price:200,
+    spaces:5,
+
+  
+    }, 
+    {
+      id:2,
+      subject:'English',
+    location:'Lagos',
+    price:300,
+    spaces:5,
+
+    
+    },
+     {
+       id:3,
+      subject:'Physics',
+    location:'Abuja',
+    price:200,
+    spaces:5,
+
+  
+    },
+     {
+       id:4,
+      subject:'Chemistry',
+    location:'Benin',
+    price:190,
+    spaces:5,
+
+    
+    },
+     {
+       id:5,
+      subject:'Biology',
+    location:'Zaria',
+    price:150,
+    spaces:5,
+
+    },
+     {
+       id:6,
+      subject:'Art',
+    location:'New York',
+    price:120,
+    spaces:5,
+    },
+     {
+       id:7,
+      subject:'Agriculture',
+    location:'Osun',
+    price:100,
+    spaces:5,
+    },
+     {
+       id:8,
+      subject:'Economics',
+    location:'London',
+    price:100,
+    spaces:5,
+    },
+     {
+       id:9,
+      subject:'History',
+    location:'Ogun',
+    price:90,
+    spaces:5,
+    },
+     {
+       id:10,
+      subject:'Computing',
+    location:'Lagos',
+    price:100,
+    spaces:5,
+    
+    },
+    
+  ]
+  
+                
